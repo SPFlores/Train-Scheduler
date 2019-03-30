89a88891db2af1e2a318a6d42b0aedf3a2cd3d2a
SPFlores <sflores92886@gmail.com>
2019-03-30 16:29:57 -0700

    boilerplate and table layout

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyABWFT1cb2jgViugI2OsYGDc_2esNZKqx8",
+  authDomain: "trainscheduleify.firebaseapp.com",
+  databaseURL: "https://trainscheduleify.firebaseio.com",
+  projectId: "trainscheduleify",
+  storageBucket: "",
+  messagingSenderId: "297497188221"
+}
+
+firebase.initializeApp(config)
