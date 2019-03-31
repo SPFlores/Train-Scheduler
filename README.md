3d8b9004f439dba0b9271134c5845465f30f24d2
SPFlores <sflores92886@gmail.com>
2019-03-31 15:13:21 -0700

    minutes away

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Train-Scheduler
+
+https://stackoverflow.com/questions/29745873/hour-difference-between-two-timeshhmmss-ain-momentjs
--- assets/js/app.js
+++ assets/js/app.js
@@ -24,16 +24,43 @@ db.collection('trains').onSnapshot(snap => {
   `
   snap.docs.forEach(doc => {
     let { name, destination, time, frequency } = doc.data()
-let nextArrival 
-let minutesAway
+    let firstArrival = moment(`${time}`, 'HHmm').add(`${frequency}`, 'ms')
+    let rawResult = Math.floor(firstArrival.format('HHmm') - moment().format('HHmm'))
+    let rawResultStr = rawResult.toString()
+    let nextArrival, minutesAway, hours, minutes
+
+    if (rawResult < 0) {
+      // nextArrival = time + (((moment().format('hh:mm a')).subtract(firstArrival)) / frequency ) * frequency
+      console.log('earlier than now')
+      console.log(rawResult)
+      // raw = -125 frequency = 60
+      // if absolute value of rawResult is < frequency, nextArrival += frequency - abs.rawResult HAVE TO CONVERT
+      // nextArrival = 
+    } else {
+      nextArrival = firstArrival.format('hh:mm a')
+    }
+
+    if (0 <= rawResult && rawResult < 60) {
+      minutesAway = rawResult
+    } else if (60 < rawResult && rawResult < 100) {
+      minutesAway = rawResult - 40
+    } else if (100 <= rawResult && rawResult <= 959) {
+      hours = parseInt(rawResultStr.slice(0, 1) * 60)
+      minutes = parseInt(rawResultStr.slice(1, 3))
+      minutesAway = hours + minutes
+    } else if (rawResult > 959) {
+      hours = parseInt(rawResultStr.slice(0, 2) * 60)
+      minutes = parseInt(rawResultStr.slice(2, 4))
+      minutesAway = hours + minutes
+    }
 
     let docElem = document.createElement('tr')
     docElem.innerHTML = `
          <td>${name}</td>
          <td>${destination}</td>
-         <td>${frequency}</td>
-         <td></td>
-         <td></td>
+         <td>${frequency / 60000}</td>
+         <td>${nextArrival}</td>
+         <td>${minutesAway}</td>
         `
     document.querySelector('#trainTable').append(docElem)
   })
@@ -65,7 +92,7 @@ const addNewTrain = _ => {
   db.collection('trains').doc(id).set({
     name: document.querySelector('#name').value,
     destination: document.querySelector('#destination').value,
-    time: moment(`${document.querySelector('#time').value}`, 'hmm').format('HH:mm'),
+    time: moment(`${document.querySelector('#time').value}`, 'HHmm').format('HHmm'),
     frequency: (document.querySelector('#frequency').value) * 60000
   })
 }
