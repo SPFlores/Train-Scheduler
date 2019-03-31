feeb6a103836f07d908778e74e0b095bbf8f0449
SPFlores <sflores92886@gmail.com>
2019-03-30 19:04:00 -0700

    storing in firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,8 @@ const config = {
 
 firebase.initializeApp(config)
 
+let db = firebase.firestore()
+
 document.querySelector('#submitBtn').addEventListener('click', e => {
   e.preventDefault()
   if (document.querySelector('#name').value === '') {
@@ -20,6 +22,7 @@ document.querySelector('#submitBtn').addEventListener('click', e => {
   } else if (isNaN(parseInt(document.querySelector('#frequency').value))) {
     document.querySelector('#frequencyFormat').style.display = 'inline'
   } else {
+    addNewTrain()
     document.querySelector('#nameFormat').style.display = 'none'
     document.querySelector('#name').value = ''
     document.querySelector('#destinationFormat').style.display = 'none'
@@ -28,10 +31,32 @@ document.querySelector('#submitBtn').addEventListener('click', e => {
     document.querySelector('#time').value = ''
     document.querySelector('#frequencyFormat').style.display = 'none'
     document.querySelector('#frequency').value = ''
-    addNewTrain()
   }
 })
 
 const addNewTrain = _ => {
   console.log('added new train')
+
+  let id = db.collection('users').doc().id
+
+  db.collection('trains').doc(id).set({
+    name: document.querySelector('#name').value,
+    destination: document.querySelector('#destination').value,
+    time: moment(`${document.querySelector('#time').value}`, `HH:mm`),
+    frequency: (document.querySelector('#frequency').value)*60000
+  })
+
+  //   console.log(moment(`${birthday} ${time}`, 'YYYY-MM-DD HH:mm').valueOf())
+  //   console.log(picture)
+
+  //   // console.log(moment(birthday, 'YYYY-MM-DD').format('MMMM Do, YYYY'))
+  //   // console.log(moment(time, 'HH:mm').format('hh:mm a'))
+
+  //   // storage.ref('images').child(`${id}.jpg`) // or
+  //   storage.ref(`images/${id}.jpg`).put(picture)
+
+  //   document.querySelector('#name').value = ''
+  //   document.querySelector('#email').value = ''
+  //   document.querySelector('#birthday').value = ''
+  //   document.querySelector('#time').value = ''
 }
