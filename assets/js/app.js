edaa33f25fa2e8215495ecada1daddb78d2ca9f0
SPFlores <sflores92886@gmail.com>
2019-03-30 18:33:00 -0700

    new train form

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,30 @@ const config = {
 }
 
 firebase.initializeApp(config)
+
+document.querySelector('#submitBtn').addEventListener('click', e => {
+  e.preventDefault()
+  if (document.querySelector('#name').value === '') {
+    document.querySelector('#nameFormat').style.display = 'inline'
+  } else if (document.querySelector('#destination').value === '') {
+    document.querySelector('#destinationFormat').style.display = 'inline'
+  } else if (!(document.querySelector('#time').value).includes(':')) {
+    document.querySelector('#timeFormat').style.display = 'inline'
+  } else if (isNaN(parseInt(document.querySelector('#frequency').value))) {
+    document.querySelector('#frequencyFormat').style.display = 'inline'
+  } else {
+    document.querySelector('#nameFormat').style.display = 'none'
+    document.querySelector('#name').value = ''
+    document.querySelector('#destinationFormat').style.display = 'none'
+    document.querySelector('#destination').value = ''
+    document.querySelector('#timeFormat').style.display = 'none'
+    document.querySelector('#time').value = ''
+    document.querySelector('#frequencyFormat').style.display = 'none'
+    document.querySelector('#frequency').value = ''
+    addNewTrain()
+  }
+})
+
+const addNewTrain = _ => {
+  console.log('added new train')
+}
